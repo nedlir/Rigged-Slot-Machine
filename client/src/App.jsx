86ab8b85d904db8fdb15d3d77a987ea1b120303d
nedlir <neliran2@365.openu.ac.il>
2024-07-12 16:57:26 +0200

    basic structure of the client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import { useQuery } from "react-query";
 
 const SERVER_URL = "http://localhost:5000";
 
-function App() {
-  const {
-    data: initialCredits,
-    isLoading,
-    isError,
-  } = useQuery("initialCredits", async () => {
+const App = () => {
+  const [credits, setCredits] = useState(0);
+  const [gameStarted, setGameStarted] = useState(false);
+
+  // useQuery hook to fetch initial credits
+  const { isLoading, isError } = useQuery("initialCredits", async () => {
+    console.log("Fetching initial credits...");
     const response = await axios.get(`${SERVER_URL}/api/credits`);
-    return response.data.credits;
+    console.log("Response from server:", response.data);
+    setCredits((credits) => response.data.credits); // Update initial credits from server using updater function
   });
 
-  if (isLoading) return <div>Loading...</div>;
-  if (isError) return <div>Error fetching initial credits</div>;
+  if (isLoading) {
+    console.log("Loading...");
+    return <div>Loading...</div>;
+  }
+
+  if (isError) {
+    console.log("Error fetching initial credits.");
+    return <div>Error fetching initial credits</div>;
+  }
+
+  const startGame = () => {
+    console.log("Game started!");
+    setGameStarted((gameStarted) => true);
+    // TBA
+  };
 
   return (
-    <div className="App">
-      <h1>Slot Machine Game</h1>
-      <h2>Initial Credits: {initialCredits}</h2>
-      {/*  logic will go here */}
+    <div>
+      <h1>Casino Jackpot</h1>
+
+      <div>
+        <Slots />
+      </div>
+
+      {!gameStarted && <button onClick={startGame}>Start Game</button>}
+
+      <CreditsPanel />
+      <Credits credits={credits} />
     </div>
   );
-}
+};
+
+const Slots = () => (
+  <>
+    <SlotColumn symbol="C" name="Cherry" />
+    <SlotColumn symbol="L" name="Lemon" />
+    <SlotColumn symbol="O" name="Orange" />
+  </>
+);
+
+const SlotColumn = ({ symbol, name }) => {
+  console.log(`Rendering SlotColumn for ${name}`);
+  return (
+    <div>
+      <h2>{symbol}</h2>
+      <p>{name}</p>
+    </div>
+  );
+};
+
+const CreditsPanel = () => {
+  console.log("Rendering CreditsPanel");
+  return (
+    <div>
+      <h2>CreditsPanel</h2>
+    </div>
+  );
+};
+
+const Credits = ({ credits }) => {
+  console.log("Rendering Credits");
+  return (
+    <div>
+      <h2>Current Credits: {credits}</h2>
+    </div>
+  );
+};
 
 export default App;
